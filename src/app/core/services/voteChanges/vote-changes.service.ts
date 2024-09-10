b45b94e126797a7dec1ed3d81fdd4d55ec1bddac
RolandoDev <rolando.mizhquiri@gmail.com>
2024-09-10 01:37:58 -0500

    backend consumption configuration and files configuration with methods the endpoints

--- src/app/core/services/voteChanges/vote-changes.service.ts
+++ src/app/core/services/voteChanges/vote-changes.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '@environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoteChangesService {
 
-  constructor() { }
+  private apiUrl = `${environment.apiUrl}/votesChanges`;
+
+  constructor(private http: HttpClient) {}
+
+  getVoteChanges(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  getVoteChangeById(voteChangesId: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${voteChangesId}`);
+  }
+
+  createVoteChange(voteChange: any): Observable<any> {
+    return this.http.post(this.apiUrl, voteChange);
+  }
+
+  updateVoteChange(voteChange: any): Observable<any> {
+    return this.http.put(this.apiUrl, voteChange);
+  }
+
+  deleteVoteChange(voteChangesId: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${voteChangesId}`);
+  }
 }
